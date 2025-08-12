eb48ad7740df32f187d36fdf6241f7e22d6ffa91
nadim-shahriar <swajan321@gmail.com>
2025-08-12 20:20:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) =>{
+    res.send('boss is sitting')
+})
+
+app.listen(port, ()=>{
+    console.log(`Boss-bistro isd running on Port ${port}`);
+})
