a6e82119b936b8fa4c16202da7c68a32ac83626f
nadim-shahriar <swajan321@gmail.com>
2025-08-21 18:12:34 +0600

    Cart Added

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { use } = require('react');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hucau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,14 +32,44 @@ async function run() {
 
         const menuCollection = client.db("bistroDb").collection("menu")
         const reviewCollection = client.db("bistroDb").collection("reviews")
+        const cartCollection = client.db("bistroDb").collection("carts")
+        const userCollection = client.db("bistroDb").collection("users")
 
 
+        // user related Api
+        app.get('/users', async(req, res)=>{
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+        // menu related Api
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+        // Cart related APIs
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
 
